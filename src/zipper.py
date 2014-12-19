01be749a0abfae6dce9418db0947ce60fa152d33
root <ewalbridge@gmail.com>
2014-12-19 16:51:28 -0500

    version 1.01

--- src/zipper.py
+++
@@ -1,9 +0,0 @@
-import StringIO, gzip, io
-
-def compress(value):
-    out = gzip.zlib.compress(value, 9)
-    return out
-
-def decompress(value):
-    out = gzip.zlib.decompress(value)
-    return out
